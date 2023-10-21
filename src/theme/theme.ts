d92273f1187842ccd984b1aa47d5ebbbff0976a2
Anastasiia Sorina <anastasiiasorina@gmail.com>
2023-10-21 22:47:44 +0100

    Update theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -122,26 +122,6 @@ const theme = createTheme({
           src: local('Raleway'), local('Raleway'), url(${Raleway}) format('truetype');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
-    `},
-    MuiButton: MuiButtonConfig,
-    MuiCssBaseline: {
-      styleOverrides: `
-      @font-face {
-        font-family: 'Dosis';
-        font-style: normal;
-        font-display: swap;
-        font-weight: 400;
-        src: local('Dosis'), local('Dosis'), url(${Dosis}) format('truetype');
-        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-      }
-        @font-face {
-          font-family: 'FugazOne';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('FugazOne'), local('FugazOne-Regular'), url(${FugazOne}) format('truetype');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
       `
     },
     MuiTab: {
