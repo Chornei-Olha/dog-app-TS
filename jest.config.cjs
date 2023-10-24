cecb4051a0e5331d39bce48a64cb89e7bd2f74ad
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-10-24 15:59:46 +0300

    fix: fix path jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
       '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform'
   },
   moduleNameMapper: {
-      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
-  },
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/src/test/__mocks__/fileMock.js'
+}, 
   snapshotSerializers: ['@emotion/jest/serializer']
 }
