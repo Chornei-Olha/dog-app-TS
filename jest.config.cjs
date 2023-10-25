4e0d0b80aa90c905e8f1f35f8513b7d74b7e6671
Olha Chornei <pradocw8053@gmail.com>
2023-10-25 22:32:24 +0300

    feat: added some lines

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/test/__mocks__/fileMock.js'
+      '<rootDir>/src/test/__mocks__/fileMock.js',
+    '\\.(svg\\?react)$': '<rootDir>/src/test/__mocks__/fileMock.js'
   },
   snapshotSerializers: ['@emotion/jest/serializer']
 };
