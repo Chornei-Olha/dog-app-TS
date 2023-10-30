70a855ba971d437a9f64c60c92b31b2d53204b1f
Olha Chornei <pradocw8053@gmail.com>
2023-10-30 22:17:14 +0200

    new commit

--- src/components/atoms/ModalDialog/index.tsx
+++ src/components/atoms/ModalDialog/index.tsx
@@ -4,12 +4,14 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 
 interface ModalDialogProps {
   open: boolean;
   onClose: () => void;
 }
+
 const StyledDialog = styled(Dialog)({
   display: 'flex',
   alignItems: 'center',
@@ -20,8 +22,7 @@ const StyledDialog = styled(Dialog)({
 });
 
 const StyledDialogTitle = styled(DialogTitle)({
-  fontSize: '40px',
-  backgroundColor: 'white'
+  fontSize: '40px'
 });
 
 // eslint-disable-next-line react/prop-types
