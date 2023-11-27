53d8c142e6f638f946a47c32e01da9dbe5e544b1
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-11-27 16:29:13 +0200

    feat: added settings for test coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform'
@@ -10,5 +11,22 @@ module.exports = {
       '<rootDir>/src/test/__mocks__/fileMock.js',
     '\\.(svg\\?react)$': '<rootDir>/src/test/__mocks__/fileMock.js'
   },
-  snapshotSerializers: ['@emotion/jest/serializer']
+  snapshotSerializers: ['@emotion/jest/serializer'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{ts,tsx,js,jsx}',
+    '!src/**/*.test.{ts,tsx,js,jsx}',
+    '!src/**/*.spec.{ts,tsx,js,jsx}',
+    '!src/tests/**/*.{ts,tsx,js,jsx}',
+    '!src/stories/**/*.{ts,tsx,js,jsx}'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      line: 90,
+      statements: 90
+    }
+  }
 };
